f5fec62659443ce55bb908e5c4419fd183285fa7
Florian Heinze <florian.heinze@sandstorm-media.de>
2017-03-05 15:49:49 +0100

    FEATURE: added google calendar integration

--- Packages/Sites/Konglomerat.Website/Resources/Public/JavaScript/app.js
+++ Packages/Sites/Konglomerat.Website/Resources/Public/JavaScript/app.js
@@ -11,4 +11,32 @@ $(document).ready(function(){
         $('body').removeClass('application--main-menu-is-open');
     });
 
+
+
+    $('.full-calendar').each(function() {
+        const calendarId = $(this).data('calendar-id');
+        const apiKey = $(this).data('api-key');
+        let header = {
+            left: 'title',
+            center: '',
+            right: 'month,listWeek today prev,next'
+        };
+
+        let defaultView = 'month';
+
+        if($(window).width() < 800){
+            header = null;
+            defaultView = 'listWeek';
+        }
+
+        $(this).fullCalendar({
+            googleCalendarApiKey: apiKey,
+            events: {
+                googleCalendarId: calendarId
+            },
+            defaultView: defaultView,
+            header: header,
+            firstDay: 1
+        });
+    })
 });
