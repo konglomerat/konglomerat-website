321527694f66476c8f3758a2494a5bd906fe65cb
Florian Heinze <florian.heinze@sandstorm-media.de>
2017-05-04 21:39:06 +0200

    FEATURE: added support multiple google calendars

--- Packages/Sites/Konglomerat.Website/Resources/Public/JavaScript/app.js
+++ Packages/Sites/Konglomerat.Website/Resources/Public/JavaScript/app.js
@@ -1,3 +1,25 @@
+const colorScheme = [
+    '#ea3c00',
+    '#007dea',
+    '#ea7c00',
+    '#7200ea',
+    '#ea00c0',
+    '#ead900',
+    '#00eab4',
+    '#3bea00',
+    '#eaaa00',
+    '#ea001c',
+    '#c9ea00',
+    '#0009ea'
+];
+
+// needed to correctly init java script in the backend
+if (typeof document.addEventListener === 'function') {
+    document.addEventListener('Neos.PageLoaded', function(event) {
+        initCalendar();
+    }, false);
+}
+
 $(document).ready(function(){
     $('body').on('click', '.main-menu-toggle',function () {
         $('body').toggleClass('application--main-menu-is-open');
@@ -7,9 +29,23 @@ $(document).ready(function(){
         $('body').removeClass('application--main-menu-is-open');
     });
 
+    initCalendar();
+});
+
+const initCalendar = function(){
     $('.full-calendar').each(function() {
-        const calendarId = $(this).data('calendar-id');
         const apiKey = $(this).data('api-key');
+        const calendarIdsArray = $(this).data('calendar-id').split(',');
+
+        let eventSources = [];
+
+        for (let i = 0; i < calendarIdsArray.length; i++) {
+            eventSources.push({
+                googleCalendarId: calendarIdsArray[i],
+                color: colorScheme[i]
+            });
+        }
+
         let header = {
             left: 'title',
             center: '',
@@ -25,12 +61,10 @@ $(document).ready(function(){
 
         $(this).fullCalendar({
             googleCalendarApiKey: apiKey,
-            events: {
-                googleCalendarId: calendarId
-            },
+            eventSources: eventSources,
             defaultView: defaultView,
             header: header,
             firstDay: 1
         });
     })
-});
+};
